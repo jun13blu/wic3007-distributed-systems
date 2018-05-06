97f7d746a2745f8cc6baa5c83c48458b136de5ed
Xin Zhe Khooi <khooi8913@gmail.com>
2018-05-06 22:59:30 +0800

    added sharedfs - ceph usage

---
+++ Assignment/SharedFS/daxgen2.py
@@ -0,0 +1,105 @@
+import os
+import os.path
+from os import listdir
+from os.path import isfile, join
+import pwd
+import sys
+import time
+import re
+
+from Pegasus.DAX3 import *
+
+USER = pwd.getpwuid(os.getuid())[0]
+
+# Generate the mDiffExec file names
+diffs_tbl = os.path.join(os.getcwd(), "output/diffs.tbl")
+f = open(diffs_tbl, 'r')
+diff_file_list = re.findall(r'(diff\..*\.fits)', f.read())
+new_diff_file_list = [f[:-5] + "_area.fits" for f in diff_file_list]
+
+onlyfiles = [f for f in listdir("input/rawdir") if isfile(join("input/rawdir", f))]
+new_file_names = [("hdu0_" + f) for f in onlyfiles]
+new_file_names_area = [("hdu0_" + f[:-5] + "_area.fits") for f in onlyfiles]
+
+# Create an abstract dag
+dax = ADAG("m101_part2")
+
+# Add some workflow level metadata
+dax.metadata("creator", "%s@%s" % (USER, os.uname()[1]))
+dax.metadata("created", time.ctime())
+
+# Execution continues
+mDiffExec = Job("mDiffExec")
+mDiffExec.addArguments("-p", ".", "diffs.tbl", "template.hdr", ".")
+for f in new_file_names: mDiffExec.uses(f, link=Link.INPUT, register=True, transfer=True)
+for f in new_file_names_area: mDiffExec.uses(f, link=Link.INPUT, transfer=True, register=True)
+mDiffExec.uses("diffs.tbl", link=Link.INPUT, transfer=True, register=True)
+mDiffExec.uses("template.hdr", link=Link.INPUT, transfer=True, register=True)
+for f in diff_file_list: mDiffExec.uses(f, link=Link.OUTPUT, register=False, transfer=True)
+for f in new_diff_file_list: mDiffExec.uses(f, link=Link.OUTPUT, register=False, transfer=True)
+dax.addJob(mDiffExec)
+
+mFitExec = Job("mFitExec")
+mFitExec.addArguments("diffs.tbl", "fits.tbl", ".")
+mFitExec.uses("diffs.tbl", link=Link.INPUT, transfer=True, register=True)
+for f in diff_file_list: mFitExec.uses(f, link=Link.INPUT, register=True, transfer=True)
+mFitExec.uses("fits.tbl", link=Link.OUTPUT, transfer=False, register=True)
+dax.addJob(mFitExec)
+
+mBgModel = Job("mBgModel")
+mBgModel.addArguments("images.tbl", "fits.tbl", "corrections.tbl")
+mBgModel.uses("images.tbl", link=Link.INPUT, transfer=True, register=True)
+mBgModel.uses("fits.tbl", link=Link.INPUT, transfer=True, register=True)
+mBgModel.uses("corrections.tbl", link=Link.OUTPUT, transfer=False, register=True)
+dax.addJob(mBgModel)
+
+# Replaces mBgExec
+jobs = []
+for a,b in zip(new_file_names, new_file_names_area):
+	mBackground = Job("mBackground")
+	mBackground.uses(a, link=Link.INPUT, transfer=True, register=True)
+	mBackground.uses(b, link=Link.INPUT, transfer=True, register=True)
+	mBackground.uses("out_" + a, link=Link.OUTPUT, transfer=False, register=True)
+	mBackground.uses("out_" + b, link=Link.OUTPUT, transfer=False, register=True)
+	mBackground.uses("corrections.tbl", link=Link.INPUT, transfer=True, register=True)
+	mBackground.uses("images.tbl", link=Link.INPUT, transfer=True, register=True)
+	mBackground.addArguments("-t", a, "out_" + a, "images.tbl", "corrections.tbl")
+	jobs.append(mBackground)
+	dax.addJob(mBackground)
+
+mImgtbl = Job("mImgtbl")
+mImgtbl.addArguments(".", "updated_images.tbl")
+for f in new_file_names: mImgtbl.uses("out_" + f, link=Link.INPUT, register=True, transfer=True)
+for f in new_file_names_area: mImgtbl.uses("out_" + f, link=Link.INPUT, transfer=True, register=True)
+mImgtbl.uses("updated_images.tbl", link=Link.OUTPUT, transfer=False, register=True)
+dax.addJob(mImgtbl)
+
+mAdd = Job("mAdd")
+mAdd.addArguments("-p", ".", "updated_images.tbl", "template.hdr", "m101_mosaic.fits")
+mAdd.uses("updated_images.tbl", link=Link.INPUT, transfer=True, register=True)
+mAdd.uses("template.hdr", link=Link.INPUT, transfer=True, register=True)
+for f in new_file_names: mAdd.uses("out_" + f, link=Link.INPUT, register=True, transfer=True)
+for f in new_file_names_area: mAdd.uses("out_" + f, link=Link.INPUT, transfer=True, register=True)
+mAdd.uses("m101_mosaic.fits", link=Link.OUTPUT, transfer=False, register=True)
+dax.addJob(mAdd)
+
+mJPEG = Job("mJPEG")
+mJPEG.addArguments("-gray", "m101_mosaic.fits", "0s", "max", "gaussian-log", "-out", "m101_mosaic.jpg")
+mJPEG.uses("m101_mosaic.fits", link=Link.INPUT, transfer=True, register=True)
+mJPEG.uses("m101_mosaic.jpg", link=Link.OUTPUT, transfer=True, register=False)
+dax.addJob(mJPEG)
+
+dax.depends(parent=mDiffExec, child=mFitExec)
+dax.depends(parent=mFitExec, child=mBgModel)
+for j in jobs:
+	dax.depends(parent=mBgModel, child=j)
+	dax.depends(parent=j, child=mImgtbl)
+dax.depends(parent=mImgtbl, child=mAdd)
+dax.depends(parent=mAdd, child=mJPEG)
+
+f = open("part2.dax", "w")
+dax.writeXML(f)
+f.close()
+print "Generated part2.dax"
+
+# End of m101 part 2
---
+++ Assignment/SharedFS/master_dax.py
@@ -0,0 +1,146 @@
+import os
+import os.path
+from os import listdir
+from os.path import isfile, join
+import pwd
+import sys
+import time
+import re
+from Pegasus.DAX3 import *
+
+# Download and extract file
+os.system("curl http://montage.ipac.caltech.edu/docs/m101Example/tutorial-initial.tar.gz -o gg.zip")
+os.system("tar -xvf gg.zip")
+
+# Create directories and file preparation
+os.system("mkdir input")
+os.system("cp -r m101/rawdir input")
+os.system("cp  m101/template.hdr input")
+
+# Write transformation catalogue
+def tc(dp):
+	tmp = '''
+tr {} {{
+	site condorpool {{
+		pfn "/usr/bin/{}"
+		arch "x86_64"
+		os "LINUX"
+		type "INSTALLED"
+	}}
+}}	
+	'''.format(dp,dp)
+	return tmp
+
+dependency = ["mImgtbl","mProjExec","mAdd","mBgModel","mFitExec","mJPEG","mBackground","mDiffExec","mOverlaps"]
+
+with open('tc.txt', 'w+') as f:
+	for d in dependency:
+		f.write(tc(d))
+
+# Write Pegasus Properties 
+def properties():
+	tmp = '''
+# This tells Pegasus where to find the Site Catalog
+pegasus.catalog.site.file=sites.xml
+
+# This tells Pegasus where to find the Replica Catalog
+pegasus.catalog.replica=File
+pegasus.catalog.replica.file=rc.txt
+
+# This tells Pegasus where to find the Transformation Catalog
+pegasus.catalog.transformation=Text
+pegasus.catalog.transformation.file=tc.txt
+
+# Use condor to transfer workflow data
+pegasus.data.configuration=condorio
+
+# This is the name of the application for analytics
+pegasus.metrics.app=pegasus-tutorial
+	'''
+	return tmp
+
+with open('pegasus.properties','w+') as f:
+	f.write(properties())
+
+# Write sites.xml
+def sites():
+	tmp = '''<?xml version="1.0" encoding="UTF-8"?>
+<sitecatalog xmlns="http://pegasus.isi.edu/schema/sitecatalog" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://pegasus.isi.edu/schema/sitecatalog http://pegasus.isi.edu/schema/sc-4.1.xsd" version="4.1">
+
+    <!-- The local site contains information about the submit host -->
+    <site handle="local" arch="x86_64" os="LINUX">
+        <!-- This is where intermediate data will be stored -->
+        <directory type="shared-scratch" path="/mnt/ceph/wf/scratch">
+            <file-server operation="all" url="file:///mnt/ceph/wf/scratch"/>
+        </directory>
+        <!-- This is where output data will be stored -->
+        <directory type="shared-storage" path="/mnt/ceph/wf/output">
+            <file-server operation="all" url="file:///mnt/ceph/wf/output"/>
+        </directory>
+    </site>
+
+    <site handle="condorpool" arch="x86_64" os="LINUX">
+        <!-- These profiles tell Pegasus that the site is a plain Condor pool -->
+        <profile namespace="pegasus" key="style">condor</profile>
+        <profile namespace="condor" key="universe">vanilla</profile>
+        <profile namespace="condor" key="should_transfer_files">True</profile>
+        <profile namespace="condor" key="when_to_transfer_output">ON_EXIT</profile>
+    </site>
+</sitecatalog>
+	'''
+	return tmp
+
+with open('sites.xml', 'w+') as f:
+	f.write(sites())
+
+def planner():
+	tmp = '''
+#!/bin/bash
+
+DIR=$(cd $(dirname $0) && pwd)
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 DAXFILE"
+    exit 1
+fi
+
+DAXFILE=$1
+
+# This command tells Pegasus to plan the workflow contained in
+# dax file passed as an argument. The planned workflow will be stored
+# in the "submit" directory. The execution # site is "".
+# --input-dir tells Pegasus where to find workflow input files.
+# --output-dir tells Pegasus where to place workflow output files.
+pegasus-plan --conf pegasus.properties \
+    --dax $DAXFILE \
+    --dir $DIR/submit \
+    --input-dir $DIR/input \
+    --output-dir $DIR/output \
+    --cleanup leaf \
+    --force \
+    --sites condorpool \
+    --submit
+	'''
+	return tmp
+
+with open('plan_dax.sh', 'w+') as f:
+	f.write(planner())
+
+# Part one
+os.system("python daxgen.py")
+os.system("sh plan_dax.sh part1.dax")
+
+# Wait for trigger for part two execution
+while True:
+	f = open('rc.txt', 'rw+')
+	if "diffs.tbl" in f.read():
+		break		
+	f.close()
+	time.sleep(1)
+
+# Part two
+os.system("python daxgen2.py")
+os.system("sh plan_dax.sh part2.dax")
+	
+	
