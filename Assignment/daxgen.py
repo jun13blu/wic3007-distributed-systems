a4bbd8b7f2ecf881b75a2abff3def9fe372e43b3
Xin Zhe Khooi <khooi8913@gmail.com>
2018-05-06 21:40:17 +0800

    Added M101 workflow daxgen files

---
+++ Assignment/daxgen.py
@@ -0,0 +1,78 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import pwd
+import sys
+import time
+from Pegasus.DAX3 import *
+
+USER = pwd.getpwuid(os.getuid())[0]
+
+# Register all the input files (INIT)
+onlyfiles = [f for f in listdir("input/rawdir") if isfile(join("input/rawdir", f))]
+fullpath = [os.path.join(os.getcwd(), "input/rawdir/" + f) for f in onlyfiles]
+with open('rc.txt', 'w') as f:
+	for a,b in zip(onlyfiles, fullpath):
+		f.write(a + ' file://' + b + ' site="local"\n')
+	f.write("template.hdr file://"+os.path.join(os.getcwd(),"input/template.hdr")+ ' site="local"\n')
+
+# Create an abstract dag
+dax = ADAG("m101_part1")
+
+# Add some workflow level metadata
+dax.metadata("creator", "%s@%s" % (USER, os.uname()[1]))
+dax.metadata("created", time.ctime())
+
+# Job scheduling start
+mImgtbl_rawdir_input = "."
+mImgtbl_rawdir_output = "images-rawdir.tbl"
+
+mImgtbl = Job("mImgtbl")
+mImgtbl.addArguments(mImgtbl_rawdir_input, mImgtbl_rawdir_output)
+for f in onlyfiles:	mImgtbl.uses(f, link=Link.INPUT, transfer=True, register=True)
+mImgtbl.uses(mImgtbl_rawdir_output, link=Link.OUTPUT, transfer=True, register=True)
+dax.addJob(mImgtbl)
+
+new_file_names = [("hdu0_" + f) for f in onlyfiles]
+new_file_names_area = [("hdu0_" + f[:-5] + "_area.fits") for f in onlyfiles]
+
+header_template = "template.hdr"
+mProjExec_output = "."
+stats_table = "stats.tbl"
+
+# STEP 2
+mProjExec = Job("mProjExec")
+mProjExec.addArguments("-p", mImgtbl_rawdir_input, mImgtbl_rawdir_output, header_template, mProjExec_output, stats_table)
+for f in onlyfiles:	mProjExec.uses(f, link=Link.INPUT, transfer=True, register=True)
+mProjExec.uses(mImgtbl_rawdir_output, link=Link.INPUT, transfer=True, register=True)
+mProjExec.uses(header_template, link=Link.INPUT, transfer=True, register=True)
+for f in new_file_names:	mProjExec.uses(f, link=Link.OUTPUT, transfer=True, register=True)
+for f in new_file_names_area: mProjExec.uses(f, link=Link.OUTPUT, transfer=True, register=True)
+mProjExec.uses(stats_table, link=Link.OUTPUT, transfer=True, register=True)
+dax.addJob(mProjExec)
+
+mImgtbl_output = "images.tbl"
+mImgtbl2 = Job("mImgtbl")
+mImgtbl2.addArguments(mProjExec_output, mImgtbl_output)
+for f in new_file_names:	mImgtbl2.uses(f, link=Link.INPUT, transfer=True, register=True)
+for f in new_file_names_area: mImgtbl2.uses(f, link=Link.INPUT, transfer=True, register=True)
+mImgtbl2.uses(mImgtbl_output, link=Link.OUTPUT, transfer=True, register=True)
+dax.addJob(mImgtbl2)
+
+diffs_tbl = "diffs.tbl"
+mOverlaps = Job("mOverlaps")
+mOverlaps.addArguments(mImgtbl_output, diffs_tbl)
+mOverlaps.uses(mImgtbl_output, link=Link.INPUT, transfer=True, register=True)
+mOverlaps.uses(diffs_tbl, link=Link.OUTPUT, transfer=True, register=True)
+dax.addJob(mOverlaps)
+
+dax.depends(parent=mImgtbl, child=mProjExec)
+dax.depends(parent=mProjExec, child=mImgtbl2)
+dax.depends(parent=mImgtbl2, child=mOverlaps)
+
+f = open("part1.dax", "w")
+dax.writeXML(f)
+f.close()
+print "Generated part1.dax"
+
+# End of m101 part one
---
+++ Assignment/master_dax.py
@@ -0,0 +1,114 @@
+import os
+import os.path
+from os import listdir
+from os.path import isfile, join
+import pwd
+import sys
+import time
+import re
+from Pegasus.DAX3 import *
+
+# Download and extract file
+os.system("curl http://montage.ipac.caltech.edu/docs/m101Example/tutorial-initial.tar.gz -o gg.zip | tar -xvf gg.zip")
+
+# Create directories and file preparation
+os.system("mkdir m101pegasus")
+os.system("cd m101pegasus && mkdir input")
+os.system("cp -r ../m101/input/rawdir .")
+os.system("cp ../m101/input/template.hdr .")
+
+# Write transformation catalogue
+def tc(dp):
+	tmp = '''
+	tr {} {{
+		site condorpool {{
+			pfn "/usr/bin/{}"
+			arch "x86_64"
+			os "LINUX"
+			type "INSTALLED"
+		}}
+	}}	
+	'''.format(dp,dp)
+	return tmp
+
+dependency = ["mImgTbl","mProjExec","mAdd","mBgModel","mFitExec","mJPEG","mBackground","mDiffExec"]
+
+with open('tc.txt', 'w+') as f:
+	for d in dependency:
+		f.write(tc(d))
+
+# Write Pegasus Properties 
+def properties():
+	tmp = '''
+	# This tells Pegasus where to find the Site Catalog
+	pegasus.catalog.site.file=sites.xml
+
+	# This tells Pegasus where to find the Replica Catalog
+	pegasus.catalog.replica=File
+	pegasus.catalog.replica.file=rc.txt
+
+	# This tells Pegasus where to find the Transformation Catalog
+	pegasus.catalog.transformation=Text
+	pegasus.catalog.transformation.file=tc.txt
+
+	# Use condor to transfer workflow data
+	pegasus.data.configuration=condorio
+
+	# This is the name of the application for analytics
+	pegasus.metrics.app=pegasus-tutorial
+	'''
+	return tmp
+
+with open('pegasus.properties','w+') as f:
+	f.write(properties)
+
+# Write sites.xml
+def sites():
+	tmp = '''
+	<?xml version="1.0" encoding="UTF-8"?>
+	<sitecatalog xmlns="http://pegasus.isi.edu/schema/sitecatalog" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	    xsi:schemaLocation="http://pegasus.isi.edu/schema/sitecatalog http://pegasus.isi.edu/schema/sc-4.1.xsd" version="4.1">
+
+	    <!-- The local site contains information about the submit host -->
+	    <site handle="local" arch="x86_64" os="LINUX">
+	        <!-- This is where intermediate data will be stored -->
+	        <directory type="shared-scratch" path="/tmp/wf/scratch">
+	            <file-server operation="all" url="file:///tmp/wf/scratch"/>
+	        </directory>
+	        <!-- This is where output data will be stored -->
+	        <directory type="shared-storage" path="/tmp/wf/output">
+	            <file-server operation="all" url="file:///tmp/wf/output"/>
+	        </directory>
+	    </site>
+
+	    <site handle="condorpool" arch="x86_64" os="LINUX">
+	        <!-- These profiles tell Pegasus that the site is a plain Condor pool -->
+	        <profile namespace="pegasus" key="style">condor</profile>
+	        <profile namespace="condor" key="universe">vanilla</profile>
+	        <profile namespace="condor" key="should_transfer_files">True</profile>
+	        <profile namespace="condor" key="when_to_transfer_output">ON_EXIT</profile>
+	    </site>
+	</sitecatalog>
+	'''
+	return tmp
+
+with open('sites.xml', 'w+') as f:
+	f.write(sites)
+
+# Part one
+os.system("python daxgen.py")
+os.system("./plan_dax.sh part1.dax")
+
+# Wait for trigger for part two execution
+while True:
+	f = open('rc.txt', 'r')
+	if "diffs.tbl" in f.read():
+		break		
+	f.close()
+	time.sleep(1)
+
+# Part two
+os.system("python daxgen2.py")
+os.system("./plan_dax.sh part2.dax")
+	
+	
