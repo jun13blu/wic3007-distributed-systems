b6ab23ab0a021ea2a90b42e611318923544edd36
xsvonjhs <ongseowsze@gmail.com>
2018-05-17 04:00:11 +0800

    modified workflow

--- Assignment/Pegasus/DataStaging/master_dax.py
+++ Assignment/Pegasus/DataStaging/master_dax.py
@@ -31,7 +31,7 @@ tr {} {{
 	'''.format(dp,dp)
 	return tmp
 
-dependency = ["mImgtbl","mProjExec","mAdd","mBgModel","mFitExec","mJPEG","mBackground","mDiffExec","mOverlaps"]
+dependency = ["mImgtbl","mProjExec","mAdd","mBgModel","mFitExec","mJPEG","mBackground","mDiffExec","mOverlaps","mBgExec"]
 
 with open('tc.txt', 'w+') as f:
 	for d in dependency:
